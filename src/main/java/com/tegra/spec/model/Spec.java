e91232dbda52b7d8f2bf3fed9d014ca1b729d338
Evelez23 <elmer_velez@hotmail.com>
2025-11-22 15:39:14 -0600

    Add Spec class with various attributes

---
+++ src/main/java/com/tegra/spec/model/Spec.java
@@ -0,0 +1,33 @@
+package com.tegra.spec.model;
+
+import java.util.List;
+
+public class Spec {
+
+    public String customer;
+    public String style;
+    public String colorway_number;
+    public String colorway_name;
+    public String season;
+    public String pattern;
+    public String po;
+    public String sample_type;
+    public String date;
+    public String requested_by;
+
+    public String art_image; // base64
+
+    public List<String> ink_sequence;
+
+    public String ink_type;
+    public String temp;
+    public String time;
+
+    public String placement;
+    public String dimensions;
+
+    public String instructions;
+
+    public String artist;
+    public String technician;
+}
