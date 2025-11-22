7cfa61ca6dd7cc92e4645f58275efa81d7bbf190
Evelez23 <elmer_velez@hotmail.com>
2025-11-22 15:36:28 -0600

    Create SerigraphyServiceApplication class

---
+++ src/main/java/com/tegra/spec/SerigraphyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tegra.spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerigraphyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerigraphyServiceApplication.class, args);
+    }
+}
